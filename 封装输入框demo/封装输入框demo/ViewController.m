f2fdcaaa28c76922b3a5bd0b3dfe67a53c4a8878
784846767@qq.com <784846767@qq.com>
2016-08-25 09:50:30 +0800

    .

---
+++ 封装输入框demo/封装输入框demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  封装输入框demo
+//
+//  Created by iMac on 16/8/25.
+//  Copyright © 2016年 sinfotek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
